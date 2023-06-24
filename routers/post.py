87d391205c5829dbf21f1db5bad43419797f8ab4
team2023dev <137638828+team2023dev@users.noreply.github.com>
2023-06-24 23:25:27 +0530

    first commit soumya 24-06-2023

---
+++ routers/post.py
@@ -0,0 +1,85 @@
+from .. import models,schemas,utils,oauth2
+from fastapi import FastAPI,Response,status,HTTPException,Depends,APIRouter
+from ..database import get_db
+from sqlalchemy.orm import Session
+from typing import Optional,List
+
+
+router=APIRouter(prefix='/posts')
+
+@router.get("/",response_model=List[schemas.Post])
+def get_posts(db:Session=Depends(get_db),current_user:int=Depends(oauth2.get_current_user),limit:int=10,skip:int=0,search:Optional[str]=""):
+    # cursor.execute("""SELECT * FROM posts """)
+    # posts=cursor.fetchall()
+    # print(posts)
+    posts=db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    return posts
+
+# @router.post("/createmessage")
+# def create_posts(post:Post):
+#     post_dict=post.dict()
+#     post_dict['id']=randrange(0,1000000)
+#     my_posts.append(post_dict)
+#     return {"data":post_dict}
+
+@router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
+def create_posts(post:schemas.PostCreate,db:Session=Depends(get_db),current_user:int=Depends(oauth2.get_current_user)):
+    # cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))
+    # new_post=cursor.fetchone()
+    # conn.commit()get_current_user:int=Depends(oauth2.get_current_user)
+    new_post=models.Post(owner_id=current_user.id,**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+@router.get('/{id}',response_model=schemas.Post)
+def get_posts(id:int,db:Session=Depends(get_db),current_user:int=Depends(oauth2.get_current_user)):
+    # cursor.execute("""SELECT * from posts WHERE id=%s""",(str(id)))
+    # post=cursor.fetchone()
+    # post=find_posts(id)
+    post=db.query(models.Post).filter(models.Post.id==id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} was not found")
+    return post
+
+# @router.get('/posts/latest')
+# def get_latest_post():
+#     post=my_posts[len(my_posts)-1]
+#     return post
+
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_posts(id:int,db:Session=Depends(get_db),current_user:int=Depends(oauth2.get_current_user)):
+    # index=find_index_post(id)
+    # cursor.execute("""DELETE FROM posts WHERE id = %s returning *""",(str(id)))
+    # deleted_post=cursor.fetchone()
+    # conn.commit()
+    post_query=db.query(models.Post).filter(models.Post.id==id)
+    post=post_query.first()
+    if post.first()==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'post with id: {id} does not exist')
+    if post.owner_id!=current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not authorized to perform requested action")
+    post_query.delete(synchronize_session=False)
+    # my_posts.pop(index)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put('/{id}',response_model=schemas.Post)
+def update_posts(id:int,post:schemas.PostCreate,db:Session=Depends(get_db),current_user:int=Depends(oauth2.get_current_user)):
+    # index=find_index_post(id)
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id= %s RETURNING *""",(post.title,post.content,post.published,str(id)))
+    # updated_post=cursor.fetchone()
+    # conn.commit()
+    post_query=db.query(models.Post).filter(models.Post.id==id)
+    post=post_query.first()
+    if post==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'post with id: {id} does not exist')
+    if post.owner_id!=current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not authorized to perform requested action")
+    post_query.update(post.dict(),synchronize_session=False)
+    db.commit()
+    # post_dict=post.dict()
+    # post_dict['id']=id
+    # my_posts[index]=post_dict
+    return post_query.first()
